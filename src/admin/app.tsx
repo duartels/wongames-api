14e472e116de95c03aab9b6c3645c8615422815c
Luan Duarte <luansouza789@gmail.com>
2024-03-06 15:22:45 -0300

    custom icons and texts added

---
+++ src/admin/app.tsx
@@ -0,0 +1,26 @@
+import Icon from './extensions/icon.png'
+import Logo from './extensions/logo.svg'
+
+export default {
+  config: {
+    auth: {
+      logo: Logo
+    },
+    head: {
+      favicon: Icon
+    },
+    locales: [],
+    menu: {
+      logo: Icon
+    },
+    translations: {
+      en: {
+        'Auth.form.welcome.title': 'Welcome to Won Games!',
+        'Auth.form.welcome.subtitle': 'Log in to your account',
+        'app.components.LeftMenu.navbrand.title': 'Won Games Dashboard',
+      }
+    }
+  },
+  bootstrap() {
+  },
+};
